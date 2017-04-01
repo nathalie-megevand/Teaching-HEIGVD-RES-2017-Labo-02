296bc864bafd609907ad5bbbdffc5a4a373b195f
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2017-04-01 10:35:36 +0200

    Remove implementation files introduced by a student

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -20,128 +20,51 @@ import java.util.logging.Logger;
  * This class implements the client side of the protocol specification (version 1).
  * 
  * @author Olivier Liechti
- * @author Luca Sivillica
- * @author Dany Tchente
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
-  private static String CHARSET = "UTF-8";
-
-  private Socket clientSocket;
-  private BufferedReader reader;
-  private PrintWriter writer;
-
   @Override
   public void connect(String server, int port) throws IOException {
-    clientSocket = new Socket(server, port);
-
-    reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), CHARSET));
-    writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), CHARSET));
-
-    reader.readLine(); // Read the welcome message of the server
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void disconnect() throws IOException {
-    writer.println(RouletteV1Protocol.CMD_BYE);
-    writer.flush();
-
-    reader.close();
-    writer.close();
-
-    clientSocket.close();
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public boolean isConnected() {
-    if (clientSocket != null) {
-      return clientSocket.isConnected();
-    }
-
-    return false;
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void loadStudent(String fullname) throws IOException {
-    writer.println(RouletteV1Protocol.CMD_LOAD);
-    writer.flush();
-
-    reader.readLine(); // Read the message : "Send your data [end with ENDOFDATA]" of the server
-
-    writer.println(fullname);
-    writer.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
-    writer.flush();
-
-    reader.readLine(); // Read the message "DATALOADED" of the server
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void loadStudents(List<Student> students) throws IOException {
-    writer.println(RouletteV1Protocol.CMD_LOAD);
-    writer.flush();
-
-    reader.readLine(); // Read the message : "Send your data [end with ENDOFDATA]" of the server
-
-    for (Student student : students) {
-      writer.println(student.getFullname());
-    }
-
-    writer.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
-    writer.flush();
-
-    reader.readLine(); // Read the message "DATALOADED" of the server
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public Student pickRandomStudent() throws EmptyStoreException, IOException {
-    String serverResponseJson;
-    RandomCommandResponse serverResponse;
-
-    writer.println(RouletteV1Protocol.CMD_RANDOM);
-    writer.flush();
-
-    serverResponseJson = reader.readLine();
-
-    serverResponse = JsonObjectMapper.parseJson(serverResponseJson, RandomCommandResponse.class);
-
-    /* if there is an error then we throw an exception */
-    if (!serverResponse.getError().isEmpty()) {
-      throw new EmptyStoreException();
-    }
-
-    return new Student(serverResponse.getFullname());
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public int getNumberOfStudents() throws IOException {
-    String serverResponseJson;
-    InfoCommandResponse serverResponse;
-
-    writer.println(RouletteV1Protocol.CMD_INFO);
-    writer.flush();
-
-    serverResponseJson = reader.readLine();
-
-    serverResponse = JsonObjectMapper.parseJson(serverResponseJson, InfoCommandResponse.class);
-
-    return serverResponse.getNumberOfStudents();
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public String getProtocolVersion() throws IOException {
-    String serverResponseJson;
-    InfoCommandResponse serverResponse;
-
-    writer.println(RouletteV1Protocol.CMD_INFO);
-    writer.flush();
-
-    serverResponseJson = reader.readLine();
-
-    serverResponse = JsonObjectMapper.parseJson(serverResponseJson, InfoCommandResponse.class);
-
-    return serverResponse.getProtocolVersion();
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
+
+
 
 }
--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -1,16 +1,9 @@
 package ch.heigvd.res.labs.roulette.net.server;
 
-import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.IStudentsStore;
-import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
-import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
-import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
-import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
-
-import java.io.*;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * This class implements the Roulette protocol (version 2).
@@ -19,75 +12,13 @@ import java.util.logging.Logger;
  */
 public class RouletteV2ClientHandler implements IClientHandler {
 
-  final static Logger LOG = Logger.getLogger(RouletteV1ClientHandler.class.getName());
-
-  private static String CHARSET = "UTF-8";
-
-  private final IStudentsStore store;
-
   public RouletteV2ClientHandler(IStudentsStore store) {
-    this.store = store;
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of gen  
   }
 
   @Override
   public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(is, CHARSET));
-    PrintWriter writer = new PrintWriter(new OutputStreamWriter(os, CHARSET));
-
-    writer.println("Hello. Online HELP is available. Will you find it?");
-    writer.flush();
-
-    String command;
-    boolean done = false;
-
-    while (!done && ((command = reader.readLine()) != null)) {
-
-      LOG.log(Level.INFO, "COMMAND: {0}", command);
-
-      switch (command.toUpperCase()) {
-
-        case RouletteV2Protocol.CMD_RANDOM:
-          RandomCommandResponse rcResponse = new RandomCommandResponse();
-
-          try {
-            rcResponse.setFullname(store.pickRandomStudent().getFullname());
-          } catch (EmptyStoreException ex) {
-            rcResponse.setError("There is no student, you cannot pick a random one");
-          }
-
-          writer.println(JsonObjectMapper.toJson(rcResponse));
-          writer.flush();
-          break;
-
-        case RouletteV2Protocol.CMD_HELP:
-          writer.println("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
-          break;
-
-        case RouletteV2Protocol.CMD_INFO:
-          InfoCommandResponse response = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
-          writer.println(JsonObjectMapper.toJson(response));
-          writer.flush();
-          break;
-
-        case RouletteV2Protocol.CMD_LOAD:
-          writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
-          writer.flush();
-          store.importData(reader);
-          writer.println(RouletteV2Protocol.RESPONSE_LOAD_DONE);
-          writer.flush();
-          break;
-
-        case RouletteV2Protocol.CMD_BYE:
-          done = true;
-          break;
-
-        default:
-          writer.println("Huh? please use HELP if you don't know what commands are available.");
-          writer.flush();
-          break;
-      }
-      writer.flush();
-    }
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
 }
